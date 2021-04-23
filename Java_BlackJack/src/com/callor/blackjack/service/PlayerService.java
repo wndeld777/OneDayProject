9999a2961d7ff149c01a19145a810a117591d027
wndeld777 <wndeld777@gmail.com>
2021-04-23 16:40:32 +0900

    blackjack

--- Java_BlackJack/src/com/callor/blackjack/service/PlayerService.java
+++ Java_BlackJack/src/com/callor/blackjack/service/PlayerService.java
@@ -9,16 +9,13 @@ import com.callor.blackjack.model.DeckVO;
 public class PlayerService {
 	// 각 플레이어(딜러포함)가 받은 카드를 저장할 List
 	List<DeckVO> myDeckList;
-	Scanner scan = new Scanner(System.in);
-	DeckServiceV1 dS = new DeckServiceV1();
+
 	// deck에서 한장씩 플레이어에게 전달할 카드들, 공용카드덱
 	List<DeckVO> pubDeckList;
 	String playerName;
-	List<DeckVO> deckList;
 
 	// 플레이어 이름이 없는 상태로 생성이 되면 이것을 "딜러"로 사용
 	public PlayerService(List<DeckVO> deckList) {
-
 		this(deckList, "딜러");
 	}
 
@@ -42,17 +39,11 @@ public class PlayerService {
 
 	// 각 플레이어들이 hit를 했을때 수행할 method
 	public void hit() {
-		// playerName이 "딜러" 이고 점수계산의 합이 16점이 넘으면,
-		if (this.playerName.equals("딜러") && this.sumValue() > 16) {
-			System.out.println("딜러점수 : " + this.sumValue());
-			System.out.println("딜러 Hit 금지!!");
-		} else {
-			// 아니면 받은카드를 카드 리스트에 추가(내 카드리스트에 pubDeckList의 0번째 카드를 더함)
-			myDeckList.add(pubDeckList.get(0));
+		// 받은카드를 카드 리스트에 추가(내 카드리스트에 pubDeckList의 0번째 카드를 더함)
+		myDeckList.add(pubDeckList.get(0));
 
-			// 받을 카드에서 0번째 한장 제외
-			pubDeckList.remove(0);
-		}
+		// 받을 카드에서 0번째 한장 제외
+		pubDeckList.remove(0);
 
 		System.out.println("=".repeat(50));
 		System.out.println(playerName);
@@ -67,72 +58,84 @@ public class PlayerService {
 
 	}
 
+	// 게임 실행
 	public void playGame() {
+		Scanner scan = new Scanner(System.in);
+		DeckServiceV1 dS = new DeckServiceV1();
 		dS.makeDeck();
-		deckList = dS.getDeck();
+		List<DeckVO> deckList = dS.getDeck();
+
 		PlayerService 딜러 = new PlayerService(deckList);
-		PlayerService 게이머 = new PlayerService(deckList, "게이머");
+
+		PlayerService 플레이어 = new PlayerService(deckList, "플레이어");
 
 		System.out.println("=".repeat(50));
 		System.out.println("게임을 시작합니다");
 
 		딜러.hit();
-		게이머.hit();
-
+		플레이어.hit();
 		딜러.hit();
-		게이머.hit();
+		플레이어.hit();
 
+		while (딜러.sumValue() < 17) {
+			딜러.hit();
+			continue;
+		}
+		System.out.println("=".repeat(50));
+		System.out.println("딜러의 턴이 끝났습니다");
 		System.out.println("-".repeat(50));
 		System.out.println("딜러의 점수 : " + 딜러.sumValue());
-		System.out.println("게이머의 점수 : " + 게이머.sumValue());
+		System.out.println("플레이어의 점수 : " + 플레이어.sumValue());
+		if (딜러.sumValue() == 21) {
+			System.out.println("BlackJack!!딜러 Win!!");
+			return;
+		} else if (딜러.sumValue() > 21) {
+			System.out.println("딜러 LOSS!!");
+			return;
+		}
+
 		while (true) {
-			System.out.println("게이머가 한장을 더 뽑으시겠습니까? ( YES:1 NO:2 )");
+			System.out.println("-".repeat(50));
+			System.out.println("플레이어가 한장을 더 뽑으시겠습니까? ( YES:1 NO:2 )");
 			System.out.print(">> ");
 			Integer selectNum = scan.nextInt();
 			if (selectNum == 1) {
-				게이머.hit();
-				if (게이머.sumValue() > 21) {
-					System.out.println("게이머 패!!");
+				플레이어.hit();
+				System.out.println();
+				System.out.println("딜러의 점수 : " + 딜러.sumValue());
+				System.out.println("플레이어의 점수 : " + 플레이어.sumValue());
+				if (플레이어.sumValue() == 21) {
+					System.out.println("BlackJack!! 플레이어 Win!!");
 					break;
-				} else if (게이머.sumValue() == 21) {
-					System.out.println("Blackjack 게이머 승!!");
+				} else if (플레이어.sumValue() < 21) {
+					continue;
+				} else if (딜러.sumValue() > 플레이어.sumValue()) {
+					System.out.println("딜러 Win!!");
 					break;
-				}
-				continue;
-			} else if (selectNum == 2) {
-				System.out.println("게이머의 턴이 끝났습니다");
-			}
-			for (int i = 0; i < 17; i++) {
-				if (딜러.sumValue() <= i) {
-					System.out.println("딜러가 한장을 더 뽑습니다");
-					딜러.hit();
+				} else if (플레이어.sumValue() > 21) {
+					System.out.println("딜러 Win!!");
+					break;
 				}
 			}
 
-			if (딜러.sumValue() > 21) {
-				System.out.println("딜러의 턴이 끝났습니다");
-				System.out.println("딜러 패!!");
-				break;
-			} else if (딜러.sumValue() == 21) {
-				System.out.println("Blackjack 딜러 승!!");
-				break;
-			}else if (딜러.sumValue() == 게이머.sumValue()) {
-				System.out.println("비겼습니다");
-				break;
-			}
-			if (딜러.sumValue() > 16 && 딜러.sumValue() < 22) {
-				if (딜러.sumValue() > 게이머.sumValue()) {
-					System.out.println("딜러가 이겼습니다");
+			if (selectNum == 2) {
+				System.out.println();
+				System.out.println("딜러의 점수 : " + 딜러.sumValue());
+				System.out.println("플레이어의 점수 : " + 플레이어.sumValue());
+				if (딜러.sumValue() == 플레이어.sumValue()) {
+					System.out.println("딜러와 플레이어의 점수가 같습니다");
+					System.out.println("무승부!!!");
 					break;
-				} else if (딜러.sumValue() < 게이머.sumValue()) {
-					System.out.println("게이머가 이겼습니다");
+				} else if (딜러.sumValue() > 플레이어.sumValue()) {
+					System.out.println("딜러 Win!!");
+					break;
+				} else {
+					System.out.println("플레이어 Win!!");
 					break;
 				}
-				break;
 			}
-			
-		}
 
-	}
+		} // end while
+	} // end playGame
 
 }
